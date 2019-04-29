1b721d7a932af6fed3b336229872582de48ae403
Weikoi <huangzengrui@yahoo.com>
2019-04-29 10:43:41 +0800

    modify

--- Django_FeatureTools/urls.py
+++ Django_FeatureTools/urls.py
@@ -13,14 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from ft_model.views import index
-from ft_model.views import get_results
+from ft_model.views import get_results,no_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index),
     path('', index),
     path('get_results/', get_results),
+    # url(r'^\w+', no_page),
+    re_path(r'^\w+', no_page),
 ]
